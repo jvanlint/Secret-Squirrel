acdcf646971e2adc23fc24639870a3a1829b30df
Jason van Lint <jvanlint@mac.com>
2017-07-05 11:50:05 +1000

    Some initial test cases written.

--- Secret SquirrelTests/ModelTests.swift
+++ Secret SquirrelTests/ModelTests.swift
@@ -1,21 +1,20 @@
 //
-//  Secret_SquirrelTests.swift
-//  Secret SquirrelTests
+//  ModelTests.swift
+//  Secret Squirrel
 //
-//  Created by Jason van Lint on 29/6/17.
+//  Created by Jason van Lint on 5/7/17.
 //  Copyright © 2017 Dead Frog Studios. All rights reserved.
 //
 
 import XCTest
 @testable import Secret_Squirrel
 
-class Secret_SquirrelTests: XCTestCase {
+class ModelTests: XCTestCase {
+
+    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        
-        
     }
     
     override func tearDown() {
@@ -26,8 +25,13 @@ class Secret_SquirrelTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        
-        
+    }
+    
+    func testCodeNameModel(){
+        let testCodeName = CodeName.init(prefix: "Test", middle: "Middle", suffix: "Suffix")
+        XCTAssertTrue(testCodeName.prefix=="Test")
+        XCTAssertTrue(testCodeName.middle=="Middle")
+        XCTAssertTrue(testCodeName.suffix=="Suffix")
     }
     
     func testPerformanceExample() {

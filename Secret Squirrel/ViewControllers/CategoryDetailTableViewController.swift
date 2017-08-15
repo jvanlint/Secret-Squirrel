e97bd105e8cc5c91d143aa66d120f7c2ef30060e
Jason van Lint <jvanlint@mac.com>
2017-08-15 11:10:06 +1000

    Working add button. Now need to persist data.

--- Secret Squirrel/ViewControllers/CategoryDetailTableViewController.swift
+++ Secret Squirrel/ViewControllers/CategoryDetailTableViewController.swift
@@ -18,7 +18,35 @@ class CategoryDetailTableViewController: UITableViewController {
         
         let categoryData = CodeWordCategories()
         self.dataSource = categoryData.arrayOfWords(forCategory: self.categoryName)
-        navigationItem.title=categoryName
+        self.navigationItem.title=categoryName
+        
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add", style: .plain, target: self, action: #selector(addTapped))
+    }
+    
+    func addTapped(){
+            let alertController = UIAlertController(title: "Add Category Item", message: "Please input your new category item:", preferredStyle: .alert)
+            
+            let confirmAction = UIAlertAction(title: "Add", style: .default) { (_) in
+                if let field = alertController.textFields?[0] {
+                    // store your data
+                    self.dataSource.append(field.text!)
+                    self.tableView.reloadData()
+                } else {
+                    // user did not fill field
+                }
+            }
+            
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (_) in }
+            
+            alertController.addTextField { (textField) in
+                textField.placeholder = "New Category Item"
+            }
+            
+            alertController.addAction(confirmAction)
+            alertController.addAction(cancelAction)
+            
+            self.present(alertController, animated: true, completion: nil)
+        
     }
 
     // MARK: - Table view data source
@@ -39,7 +67,7 @@ class CategoryDetailTableViewController: UITableViewController {
 
         // Configure the cell...
         let label = cell.viewWithTag(100) as! UILabel
-        label.text = dataSource[indexPath.row]
+        label.text = dataSource[indexPath.row].capitalized
     
         return cell
     }
@@ -52,17 +80,18 @@ class CategoryDetailTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            dataSource.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.

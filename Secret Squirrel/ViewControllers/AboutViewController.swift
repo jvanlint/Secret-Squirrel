0ead49501fac700caa183b768f3bdae33e85fd02
Jason van Lint <jvanlint@mac.com>
2017-08-21 18:45:36 +1000

    Code cleanup.

--- Secret Squirrel/ViewControllers/AboutViewController.swift
+++ Secret Squirrel/ViewControllers/AboutViewController.swift
@@ -18,9 +18,11 @@ class AboutViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        lblVersion.text = AppController.sharedInstance.appVersion() + " (" + AppController.sharedInstance.appBuildNumber() + ")"
-        // Do any additional setup after loading the view.
         
+        // Update the version info on the screen.
+        lblVersion.text = AppController.sharedInstance.appVersion() + " (" + AppController.sharedInstance.appBuildNumber() + ")"
+        
+        // Animate the squirrel image and heading/subheading labels.
         squirrelImage.popIn(fromScale: 0.5, duration: 0.5, delay: 0.2, completion: nil)
         lblSecretSquirrel.popIn(fromScale: 0.5, duration: 0.5, delay: 0.3, completion: nil)
         lblSubTitle.popIn(fromScale: 0.5, duration: 0.5, delay: 0.3, completion: nil)
@@ -29,39 +31,23 @@ class AboutViewController: UIViewController {
 
 
     @IBAction func didTouchWebSiteButton(_ sender: Any) {
-        let url = URL(string: "http://www.deadfrogstudios.com")!
         
+        // Set the URL and open Safari to display website.
+        let url = URL(string: "http://www.deadfrogstudios.com")!
         UIApplication.shared.open(url, options: [:], completionHandler: nil)
     }
     
     @IBAction func didTouchTweetButton(_ sender: Any) {
+        
+        // Setup Twitter View Controller
         let vc = SLComposeViewController(forServiceType:SLServiceTypeTwitter)
         
+        // Set initial text.
         vc?.setInitialText("@deadfrogstudios. Secret Squirrel is...")
         
+        // Show view controller to user.
         self.present(vc!, animated: true, completion: nil)
-
     }
-    
-    func dialogBox()
-    {
-        let alertController = UIAlertController(title: "Destructive", message: "Simple alertView demo with Destructive and Ok.", preferredStyle: UIAlertControllerStyle.alert) //Replace UIAlertControllerStyle.Alert by UIAlertControllerStyle.alert
-        let DestructiveAction = UIAlertAction(title: "Destructive", style: UIAlertActionStyle.destructive) {
-            (result : UIAlertAction) -> Void in
-            print("Destructive")
-        }
-        
-        // Replace UIAlertActionStyle.Default by UIAlertActionStyle.default
-        let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default) {
-            (result : UIAlertAction) -> Void in
-            print("OK")
-        }
-        
-        alertController.addAction(DestructiveAction)
-        alertController.addAction(okAction)
-        self.present(alertController, animated: true, completion: nil)
-    }
-
 }
 
 
